d2265f21909a2d29f373b5d6c977ece48d192990
neil-stephen-mcgonigle <neil.mcgonigle@justice.gov.uk>
2025-11-14 09:59:58 +0000

    Update APP_API_URL to use internal network resolution

--- search/search-service.js
+++ search/search-service.js
@@ -19,7 +19,7 @@ function createSearchService({
      * @returns {Promise<object>} A promise that resolves to the search results.
      */
     async function getSearchResults(query, pageNumber, itemsPerPage) {
-        logger.info({ query, pageNumber, itemsPerPage });
+        logger.info({ query, pageNumber, itemsPerPage }, "Fetching search results");
         const opts = {
             url: `${process.env.APP_API_URL}/search/${query}/${pageNumber}/${itemsPerPage}`,
             headers: {
