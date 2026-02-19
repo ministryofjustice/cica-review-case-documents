8ed5b1389c69803ec0cd6ee7ba6dad2defa7a248
Duncan Abraham <duncan.abraham@justice.gov.uk>
2026-02-19 14:52:22 +0000

    Feature/499 document viewer pagination image view (#99)
    * feat(pagination): Implementing mojPagination component
    
    Implemented pagination component and resolved aXe-tools recommendations
    
    Co-authored-by: Duncan Abraham <duncan.abraham@cica.gov.uk>

--- components/cica/identity-bar-with-sub-nav/fixtures/title-html-with-no-subnav.js
+++ components/cica/identity-bar-with-sub-nav/fixtures/title-html-with-no-subnav.js
@@ -1,9 +1,11 @@
 const html = `
     <div class="moj-identity-bar moj-identity-bar--nested-moj-sub-navigation">
         <div class="moj-identity-bar__container">
-            <div class="moj-identity-bar__details">
-                <span class="moj-identity-bar__title"><strong>CRN: 25-711111</strong></span>
-            </div>
+            <section role="region" aria-label="Identity bar title">
+                <div class="moj-identity-bar__details">
+                    <span class="moj-identity-bar__title"><strong>CRN: 25-711111</strong></span>
+                </div>
+            </section>
         </div>
     </div>
 `;
